9112338b09494e5c3abcca1b90298aa59f115c1f
cole.shirley <cole.shirley@flexsim.com>
2021-03-26 16:36:18 -0600

    compatibility with v1/UsageService

--- src/Type/GetUsageRequest.php
+++ src/Type/GetUsageRequest.php
@@ -10,26 +10,50 @@ class GetUsageRequest implements RequestInterface
     /**
      * @var string
      */
+    private $accountId;
+
+    /**
+     * @var string
+     */
     private $orgName;
 
     /**
      * Constructor
      *
-     * @var string $orgName
+     * @var string $accountId this method is backwards compatible so you may also pass orgName
      */
-    public function __construct(string $orgName)
+    public function __construct(string $accountId)
     {
-        $this->orgName = $orgName;
+        $this->accountId = $accountId;
+        $this->orgName = $accountId;
     }
 
     /**
      * create a new instance of this class
      *
-     * @var string $orgName
+     * @var string $accountId this method is backwards compatible so you may also pass orgName
      */
-    public static function create(string $orgName)
+    public static function create(string $accountId)
     {
         return new self(...func_get_args());
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccountId()
+    {
+        return $this->accountId;
+    }
+
+    /**
+     * @param string $accountId
+     * @return $this
+     */
+    public function setAccountId($accountId)
+    {
+        $this->accountId = $accountId;
+        return $this;
     }
 
     /**
@@ -49,7 +73,4 @@ class GetUsageRequest implements RequestInterface
         $this->orgName = $orgName;
         return $this;
     }
-
-
 }
-
