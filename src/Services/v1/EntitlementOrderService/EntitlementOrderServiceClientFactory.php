8e7cf2145492ff87a7d90cdbab3b704c3e48e79c
cole.shirley <cole.shirley@flexsim.com>
2021-04-01 11:10:03 -0600

    adding service factories

---
+++ src/Services/v1/EntitlementOrderService/EntitlementOrderServiceClientFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Flexsim\FlexnetOperations\Services\v1\EntitlementOrderService;
+
+use Phpro\SoapClient\Middleware\BasicAuthMiddleware;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapEngineFactory;
+use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapOptions;
+use Phpro\SoapClient\Soap\Handler\HttPlugHandle;
+
+class EntitlementOrderServiceClientFactory
+{
+
+    public static function factory(string $wsdl, string $username, string $password): EntitlementOrderServiceClient
+    {
+        $handler = HttPlugHandle::createWithDefaultClient();
+
+        $handler->addMiddleware(new BasicAuthMiddleware($username, $password));
+
+        $engine = ExtSoapEngineFactory::fromOptionsWithHandler(
+            ExtSoapOptions::defaults($wsdl, [])
+                ->withClassMap(EntitlementOrderServiceClassmap::getCollection()),
+            $handler
+        );
+        $eventDispatcher = new EventDispatcher();
+
+        return new EntitlementOrderServiceClient($engine, $eventDispatcher);
+    }
+}
---
+++ src/Services/v1/ManageDeviceService/ManageDeviceServiceClientFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Flexsim\FlexnetOperations\Services\v1\ManageDeviceService;
+
+use Phpro\SoapClient\Middleware\BasicAuthMiddleware;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapEngineFactory;
+use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapOptions;
+use Phpro\SoapClient\Soap\Handler\HttPlugHandle;
+
+class ManageDeviceServiceClientFactory
+{
+
+    public static function factory(string $wsdl, string $username, string $password): ManageDeviceServiceClient
+    {
+        $handler = HttPlugHandle::createWithDefaultClient();
+
+        $handler->addMiddleware(new BasicAuthMiddleware($username, $password));
+
+        $engine = ExtSoapEngineFactory::fromOptionsWithHandler(
+            ExtSoapOptions::defaults($wsdl, [])
+                ->withClassMap(ManageDeviceServiceClassmap::getCollection()),
+            $handler
+        );
+        $eventDispatcher = new EventDispatcher();
+
+        return new ManageDeviceServiceClient($engine, $eventDispatcher);
+    }
+}
